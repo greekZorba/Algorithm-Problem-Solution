8b4e1d58d2d793da8ef3544cb9b360097badc330
greekZorba <smiling.jinhak@gmail.com>
2019-02-22 08:54:55 +0900

    programmers 전화번호 목록 해결

--- src/programmers/levelTwo/PhoneBookList.java
+++ src/programmers/levelTwo/PhoneBookList.java
@@ -1,7 +1,7 @@
 package programmers.levelTwo;
 
+
 import java.util.Arrays;
-import java.util.HashMap;
 
 /**
  * 전화번호 목록
@@ -10,24 +10,27 @@ public class PhoneBookList {
 
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        HashMap<String, Integer> phoneMap = new HashMap<>();
-        for(int i=0; i<phone_book.length; i++){
-            phoneMap.put(phone_book[i], Integer.parseInt(phone_book[i]));
-        }
-
-        
+        Arrays.sort(phone_book);
 
         for(int i=0; i<phone_book.length-1; i++){
 
-            for(int j=1; j<phone_book.length; j++){
-//                String temp = phone_book[j].substring(0, phone_book[i].length());
-//                String temp = phone_book[j].substring(0, 3);
-                System.out.println("____"+"1234".substring(0,3));
-                System.out.println("__length"+phone_book[i].length());
-                if(phone_book[i].equals(phone_book[j].substring(0, phone_book[i].length()))){
-                    answer = false;
+            for(int j=i+1; j<phone_book.length; j++){
+
+                if(phone_book[i].charAt(0) != phone_book[j].charAt(0)){
                     break;
                 }
+
+                String longerPhoneNum = phone_book[i].length() >= phone_book[j].length() ? phone_book[i] : phone_book[j];
+                String shorterPhoneNum = phone_book[i].length() < phone_book[j].length() ? phone_book[i] : phone_book[j];
+
+                if(shorterPhoneNum.equals(longerPhoneNum.substring(0, shorterPhoneNum.length()))){
+                        answer = false;
+                        break;
+                }
+            }
+
+            if(!answer){
+                break;
             }
         }
         return answer;
@@ -38,6 +41,7 @@ public class PhoneBookList {
 //        String[] phone_book = new String[]{"123","456","789"};
 //        String[] phone_book = new String[]{"119", "97674223", "1195524421"};
         String[] phone_book = new String[]{"12","123","1235","567","88"};
+//        String[] phone_book = new String[]{"113", "12340", "123440", "12345", "98346"};
         System.out.println(phoneBookList.solution(phone_book));
     }
 }
