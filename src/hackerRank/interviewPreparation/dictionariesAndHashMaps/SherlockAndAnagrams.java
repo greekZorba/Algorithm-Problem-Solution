c6dd8ced335d4756d57b57af5f8fbc2e104f4e7a
jinhak Kim <smiling.jinhak@gmail.com>
2019-04-21 00:42:01 +0900

    solve SherlockAndAnagrams
    와 진짜 어려웠다....

--- src/hackerRank/interviewPreparation/dictionariesAndHashMaps/SherlockAndAnagrams.java
+++ src/hackerRank/interviewPreparation/dictionariesAndHashMaps/SherlockAndAnagrams.java
@@ -1,61 +1,67 @@
 package hackerRank.interviewPreparation.dictionariesAndHashMaps;
 
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 
 public class SherlockAndAnagrams {
 
     static int sherlockAndAnagrams(String s) {
 
-
         int result = 0;
 
-        for(int i=0; i<s.length(); i++) {
+        for(int i=0; i<s.length()-1; i++) {
             int stringLength = 1;
 
-            for(int j=0; j<s.length(); j++) {
-                LinkedList<String> charCollection = new LinkedList<>();
+            for(int j=i; j<s.length()-1; j++) {
 
-                if(i+stringLength > s.length()){
-                    break;
+                LinkedList<String> perCharacter = new LinkedList<>();
+
+                // 비교할 기준이 될 문자 만듦
+                for(int k=i; k<i+stringLength; k++){
+                    perCharacter.add(String.valueOf(s.charAt(k)));
                 }
 
-                // 기준이 될 알파벳 넣기
-                for(int k=i; k<i+stringLength; k++) {
-                    charCollection.add(String.valueOf(s.charAt(k)));
-                }
+                for(int k=i+1; k<s.length(); k++){
+                    boolean matchFlag = true;
+                    LinkedList<String> dummyCharacter = new LinkedList<>();
 
-                System.out.println(charCollection.toString());
-                for(int k=j; k<s.length(); k++) {
+                    // 체크할 문자길이가 실제 문자길이보다 길어지면 break
+                    if(k+stringLength > s.length()){
+                        break;
+                    }
 
-                    boolean pairFlag = true;
-                    LinkedList<String> dummyCollection = charCollection;
+                    // 깊은 복사를 위해서 직접 for문 돌림
+                    // 비교하려는 문자열을 원상태로 되돌려 비교하기 위해
+                    for(int l=0; l<perCharacter.size(); l++) {
+                        dummyCharacter.add(perCharacter.get(l));
+                    }
 
+                    // 기준되는 문자열과 만들어지는 문자열들을 비교
                     for(int l=k; l<k+stringLength; l++) {
+                        String comparingChar = String.valueOf(s.charAt(l));
 
-                        if(dummyCollection.contains(String.valueOf(s.charAt(k)))){
+                        // 알파벳 순서가 달라도 존재만 하면 되기 때문에
+                        // 존재 유무를 체크해서 remove
+                        if(dummyCharacter.contains(comparingChar)) {
 
-                            dummyCollection.remove(String.valueOf(s.charAt(k)));
-                        }else{
-                            pairFlag = false;
+                            dummyCharacter.remove(comparingChar);
+                        }
+                        // 존재하지 않는 틀린 문자가 나오면 더이상 비교가 필요없이 Anagram에서 탈락
+                        else {
+                            matchFlag = false;
                             break;
                         }
-
                     }
 
-                    if(pairFlag){
-
+                    if(matchFlag){
                         result++;
                     }
-                }
 
+                }
                 stringLength++;
+
             }
 
         }
-
 
         return result;
     }
@@ -63,6 +69,6 @@ public class SherlockAndAnagrams {
 
     public static void main(String[] args) {
 
-        System.out.println(SherlockAndAnagrams.sherlockAndAnagrams("abcd"));
+        System.out.println(SherlockAndAnagrams.sherlockAndAnagrams("mom"));
     }
 }
